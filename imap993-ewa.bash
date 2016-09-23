99c4ebba52a35fe439381ae38905f07ca7067eeb
epnet-test <epnet@epfl.ch>
2016-09-23 14:22:25 +0200

    Microsoft

--- imap993-ewa.bash
+++ imap993-ewa.bash
@@ -1,11 +1,11 @@
 #!/bin/bash
-printf ". CAPABILITY\n. LOGOUT\n" | /usr/bin/openssl s_client -crlf -connect [$HM_SRV_IPADDR]:$HM_SRV_PORT 2>/dev/null -ign_eof | grep "* BYE IMAP4 server logging out" >/dev/null
+printf ". CAPABILITY\n. LOGOUT\n" | /usr/bin/openssl s_client -crlf -connect [$HM_SRV_IPADDR]:$HM_SRV_PORT 2>/dev/null -ign_eof | grep "* BYE Microsoft Exchange Server 2013 IMAP4 server signing off" >/dev/null
 check=$?
 #   echo "$HM_SRV_IPADDR:$HM_SRV_PORT returned: $check"
 
-if test $check == 0 ; then      
-#    echo "UP"  
- exit 0       
-else        
- exit 1       
+if test $check == 0 ; then
+#    echo "UP"
+ exit 0
+else
+ exit 1
 fi
